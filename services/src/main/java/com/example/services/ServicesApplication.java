8b151339ea2382b1da23ea41a145040b65839f3d
Robert Slade <7624638+RSlade97@users.noreply.github.com>
2022-03-31 15:15:01 -0400

    Initial commit

---
+++ services/src/main/java/com/example/services/ServicesApplication.java
@@ -0,0 +1,14 @@
+package com.example.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+public class ServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesApplication.class, args);
+	}
+
+}
